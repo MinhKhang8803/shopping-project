570deedceb45266a4af9e882fc73e4674554491c
Se-pher <hunglqgch210012@fpt.edu.vn>
2024-01-15 23:32:23 +0700

    Update Cart + style

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 // App.js
-import React from 'react';
-import { Provider } from 'react-redux';
-import store from './store/configureStore';
-import Header from './components/Header';
-import ProductList from './components/ProductList';
-import Checkout from './components/Checkout';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/configureStore";
+import Header from "./components/Header";
+import Cart from "./components/Cart"
+// import ProductList from "./components/ProductList";
+// import Checkout from "./components/Checkout";
 
 function App() {
   return (
     <Provider store={store}>
-      <div>
-        <Header />
-        <ProductList />
-        <Checkout />
-      </div>
+      <Router>
+        <div>
+          <Header />
+          <Cart />
+        </div>
+      </Router>
     </Provider>
   );
 }
